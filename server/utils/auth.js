de6884b8a4285eeecaad7c7bb293864c31e5d315
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-02-10 11:30:40 -0500

    added utils auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,1 +1,34 @@
-const card = require('../models/card');
+const jwt = require('jsonwebtoken');
+
+//set token secret and expiration date
+const secret = 'mysecretshhhhh';
+const expiration = '1h';
+
+module.exports = {
+    //function for our authenticated routes
+    authMiddleware: function (req, res, next){
+        let token = req.query.token || req. headers.authorization;
+        if (req.headers.authorization){
+            token = token
+            .split(' ')
+            .pop()
+            .trim();
+        }
+        if (!token){
+            return res.status(400).json({message: 'You have no token!'});
+        }
+        try{
+            const {data} = jwt.verify(token, secret, {maxAge: expiration});
+            req.user =data;
+        }catch {
+            console.log('Invalid token');
+            return res.status(400).json({message: 'Invalid token!'});
+        }
+        next();
+    },
+signToken: function ({username, email, _id}){
+    const payload = {username,email, _id};
+
+return jwt.sign({data: payload}, secret, {expiresIn: expiration});
+},
+};
