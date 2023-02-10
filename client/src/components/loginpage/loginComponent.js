6b6751815cabc90f936779b25e916534c7382296
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-10 13:53:43 -0500

    flipped component and page error

--- client/src/components/loginpage/loginComponent.js
+++ client/src/components/loginpage/loginComponent.js
@@ -1,10 +1,43 @@
-import Login from '../../pages/login';
 
-function SignInPage() {
+// login page
+
+// pages/Login.js
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        // Make a request to your API to log the user in
+        // ...
+        // Ask back end pros about this!!!!
+
+        // Redirect the user to the home page after a successful login
+        history.push('/');
+    };
+
     return (
-        <div>
-            <h1>Sign In</h1>
-            <Login />
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                placeholder="Email"
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                placeholder="Password"
+            />
+            <button type="submit">Login</button>
+        </form>
     );
 }
+
+export default Login;
