8ab5130b732921b46edc7d1ef8e069a2d7db5d5f
Blanket <jaketung2000@gmail.com>
2023-02-13 11:14:37 -0500

    server/apollo set up and running graphql

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/your_db_name', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
+    
 });
 
 module.exports = mongoose.connection;
