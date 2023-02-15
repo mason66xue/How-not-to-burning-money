c4b6ca3b97a6671abbf9feffd47cf1893b9101f8
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-15 10:47:50 -0500

    added layout and navigation components, still running tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import LoginForm from "./components/loginpage/LoginForm";
 import SignupForm from "./components/signup/SignupForm";
+import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
+
+const { Route, Routes } = require("react-router-dom");
+import Layout from "./components/layout/Layout";
 
 
 
@@ -13,13 +17,14 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   return (
 
-    <div>
-
-      <SignupForm />
-      <LoginForm />
-      <Dashboard />
-
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} />
+      </Routes>
+    </Layout>
 
   );
 }
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,54 @@
+import { Link } from 'react-router-dom';
+import classes from './Navbar.module.css';
+
+//create nav bar elem
+
+function Navbar() {
+
+    return (
+
+        <header className={classes.header}>
+            <div className={classes.logo}>Burning Money</div>
+            <nav>
+                <ul>
+
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+
+                    <li>
+                        <a href="#about-us">About Us</a>
+                    </li>
+
+                    <li>
+                        <a href="#testimonials">Testimonials</a>
+                    </li>
+
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+
+                    <li>
+                        <Link to='/dashboard'>My Profile</Link>
+                    </li>
+
+                    <li>
+                        <Link to='/login'>Log In</Link>
+                    </li>
+
+                    <li>
+                        <Link to='/signup'>Sign up</Link>
+                    </li>
+
+                </ul>
+            </nav>
+        </header>
+
+    );
+
+}
+
+export default Navbar;
+
+
+
