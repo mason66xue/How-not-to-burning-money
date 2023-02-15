eef57580c64bd48564c4683b2f1e58dfc028dbd8
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-15 14:24:58 -0500

    saving for pull

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+
+import LandingPageComponent from "../components/landingpage/LandingPageComponent";
+
+function LandingPage() {
+    return (
+        <div>
+            <LandingPageComponent />
+        </div>
+    );
+}
+
+export default LandingPage;
