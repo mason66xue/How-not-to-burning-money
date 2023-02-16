bb13d5561007fcc732f1f85c79d064505e0cb6b6
Blanket <jaketung2000@gmail.com>
2023-02-16 13:31:39 -0500

    heroku update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 
-const { Route, Routes } = require("react-router-dom");
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import Dashboard from "./pages/Dashboard";
 import Layout from "./components/layout/Layout";
 import LandingPage from "./pages/LandingPage";
 
+const { Route, Routes } = require("react-router-dom");
 
 
 
