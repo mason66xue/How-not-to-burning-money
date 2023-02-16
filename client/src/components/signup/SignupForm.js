5d8688aaaf610d3a960b231cdaf893acd21f4630
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-16 14:48:28 -0500

    troubleshooting login

--- client/src/components/signup/SignupForm.js
+++ client/src/components/signup/SignupForm.js
@@ -6,48 +6,90 @@ import classes from './SignupForm.module.css';
 import Card from '../userinterface/Card';
 
 import React, { useState } from 'react';
-import axios from 'axios';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../../utils/mutations';
+import Auth from '../../utils/auth';
 
 
-function SignupForm() {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+
+const SignupForm = () => {
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
     const [errorMessage, setErrorMessage] = useState('');
     const [isLoading, setIsLoading] = useState(false);
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        setIsLoading(true);
+    const [addUser, { error, data }] = useMutation(ADD_USER);
 
-        axios
-            .post('https://example.com/api/login', {
-                username,
-                email,
-                password,
-            })
-            .then(response => {
-                setIsLoading(false);
-                // Handle successful login
-            })
-            .catch(error => {
-                setIsLoading(false);
-                setErrorMessage('Incorrect username or password. Please try again.');
-            });
+
+    // update state based on form input changes
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [id]: value,
+        });
     };
+
+    // submit form
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+
+        try {
+            const { data } = await addUser({
+                variables: { ...formState },
+            });
+
+            Auth.login(data.addUser.token);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    // function SignupForm() {
+    //     const [username, setUsername] = useState('');
+    //     const [email, setEmail] = useState('');
+    //     const [password, setPassword] = useState('');
+    //     const [errorMessage, setErrorMessage] = useState('');
+    //     const [isLoading, setIsLoading] = useState(false);
+
+    //     const handleSubmit = event => {
+    //         event.preventDefault();
+    //         setIsLoading(true);
+
+    //         axios
+    //             .post('https://example.com/api/login', {
+    //                 username,
+    //                 email,
+    //                 password,
+    //             })
+    //             .then(response => {
+    //                 setIsLoading(false);
+    //                 // Handle successful login
+    //             })
+    //             .catch(error => {
+    //                 setIsLoading(false);
+    //                 setErrorMessage('Incorrect username or password. Please try again.');
+    //             });
+    //     };
 
     return (
 
         <Card>
 
-            <form className={classes.form} onSubmit={handleSubmit}>
+            <form className={classes.form} onSubmit={handleFormSubmit}>
                 <div className={classes.control}>
                     <label htmlFor="username">Name:</label>
                     <input
                         type="text"
                         id="username"
-                        value={username}
-                        onChange={event => setUsername(event.target.value)}
+                        value={formState.name}
+                        onChange={handleChange}
                     />
                 </div>
 
@@ -56,8 +98,8 @@ function SignupForm() {
                     <input
                         type="text"
                         id="email"
-                        value={email}
-                        onChange={event => setEmail(event.target.value)}
+                        value={formState.email}
+                        onChange={handleChange}
                     />
                 </div>
 
@@ -66,8 +108,8 @@ function SignupForm() {
                     <input
                         type="password"
                         id="password"
-                        value={password}
-                        onChange={event => setPassword(event.target.value)}
+                        value={formState.password}
+                        onChange={handleChange}
                     />
                 </div>
                 {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
