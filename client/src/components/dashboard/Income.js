ce935d394e0e0dd4258b532c94f34f0e71006025
Blanket <jaketung2000@gmail.com>
2023-02-16 20:55:40 -0500

    losing my mind

--- client/src/components/dashboard/Income.js
+++ client/src/components/dashboard/Income.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
 import Card from '../userinterface/Card';
-import { useMutation,useQuery } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { SET_INCOME } from '../../utils/mutations';
 import authService from '../../utils/auth';
-// import { QUERY_USER } from '../../utils/queries';
 
 function Income() {
+ 
+    const [formState, setFormState] = useState({
+        amount: 0,
+    });
 
-    // const { data:queryData } = useQuery(QUERY_USER,{
-    //     variables: { email: authService.getProfile().data.email }
-    // });
-    // const userIncome = queryData.getUser.income;
-
-    const [inputState, setInputState] = useState({ income: '' });
+    const [errorMessage, setErrorMessage] = useState('');
 
     const [setIncome, { error, data }] = useMutation(SET_INCOME, {
         onError: (error) => {
@@ -20,42 +18,38 @@ function Income() {
         }
     });
 
-
     const handleChange = (event) => {
         const { id, value } = event.target;
         console.log("id and value changes")
-        console.log(id, value);
-        console.log("++++++++++++++++++++++++++++++++++")
-        console.log("inputState before changes")
-        console.log(inputState);
-        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
-        setInputState({
-            ...inputState,
+      console.log(id, value);
+        setFormState({
+            ...formState,
             [id]: value,
         });
-        console.log("inputState after changes")
-        console.log(inputState);
-        console.log("=================================")
+        console.log("formState changes update")
+        console.log(formState);
     };
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
 
+        setErrorMessage('');
 
         try {
+            console.log("formState from handleFormSubmit")
+            console.log(formState)
             const { data } = await setIncome({
-                variables: { inputState },
+                variables: {amount:5400}
             });
             console.log("data from login mutation")
             // use authService to grab token
-            console.log(data);
-            console.log("#####################################")
-            authService.login(data.setIncome.user.token);
+            // authService.login(data.setIncome.token);
         } catch (e) {
             console.error(e);
+            setErrorMessage('Something went wrong. Please try again.');
         }
 
+        // setIsLoading(false);
     };
 
   
@@ -64,13 +58,14 @@ function Income() {
             <div>
                 <h1>Income</h1>
                 <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <form>
                     <input
                         type="number"
-                        id="income"
-                        value={inputState.income}
+                        id="amount"
+                        value={formState.amount}
                         onChange={handleChange}
                     />
-                    {inputState && ( 
+                    {formState && ( 
                         <div
                             style={{
                                 color: 'green',
@@ -78,18 +73,91 @@ function Income() {
                                 marginLeft: '10px',
                             }}
                         >
-                            {inputState.income}
+                            {formState.amount}
                         </div>
                     )}
+                    </form>
                 </div>
-                <button onClick={handleFormSubmit}>Set Income</button>
+                <button onClick={handleFormSubmit}>Set Income</button> 
+                
             </div>
+           
         </Card>
     );
 
 }
 
 export default Income;
+
+// import React, { useState } from 'react';
+// import Card from '../userinterface/Card';
+// import { useMutation, useQuery } from '@apollo/client';
+// import { SET_INCOME } from '../../utils/mutations';
+// import authService from '../../utils/auth';
+// import { QUERY_USER } from '../../utils/queries';
+
+// function Income() {
+//     const { data: queryData } = useQuery(QUERY_USER, {
+//         variables: { email: authService.getProfile().data.email },
+//     });
+//     const userIncome = queryData?.getUser?.income;
+
+//     const [inputState, setInputState] = useState({ income: userIncome || 0 });
+
+//     const [setIncome, { error, data }] = useMutation(SET_INCOME, {
+//         onError: (error) => {
+//             console.log('Error:', error.message);
+//         },
+//     });
+
+//     const handleChange = (event) => {
+//         const { id, value } = event.target;
+//         setInputState({
+//             ...inputState,
+//             [id]: value,
+//         });
+//     };
+
+//     const handleFormSubmit = async (event) => {
+//         event.preventDefault();
+//         try {
+//             const { data } = await setIncome({
+//                 variables: { income: inputState.income },
+//                 refetchQueries: [
+//                     { query: QUERY_USER, variables: { email: authService.getProfile().data.email } },
+//                 ],
+//             });
+//             authService.login(data.setIncome.user.token);
+//         } catch (e) {
+//             console.error(e);
+//         }
+//     };
+
+//     return (
+//         <Card>
+//             <div>
+//                 <h1>Income</h1>
+//                 <div style={{ display: 'flex', alignItems: 'center' }}>
+//                     <input type="number" id="income" value={inputState.income} onChange={handleChange} />
+//                     {inputState.income && (
+//                         <div
+//                             style={{
+//                                 color: 'green',
+//                                 fontSize: '24px',
+//                                 marginLeft: '10px',
+//                             }}
+//                         >
+//                             {inputState.income}
+//                         </div>
+//                     )}
+//                 </div>
+//                 <button onClick={handleFormSubmit}>Set Income</button>
+//             </div>
+//         </Card>
+//     );
+// }
+
+// export default Income;
 
 
 
